afa34628cdada5c2f8b14ac037e6f8a671779ff7
MichaelSterzinger <michael7sterzinger@gmail.com>
2021-12-28 23:54:36 +0100

    lang & prefix fix

--- utils.py
+++ utils.py
@@ -3,11 +3,11 @@ def get_stdforms(entity, data):
 
     def get_query(entity, language):
         q = f"""
-                PREFIX rdf: <http://www.w3.org/2000/01/rdf-schema#>
                 PREFIX wd: <http://www.wikidata.org/entity/>
+                PREFIX rdf: <http://www.w3.org/2000/01/rdf-schema#>
                 SELECT ?name
                 WHERE {{ wd:{entity} rdfs:label ?name.
-                    FILTER langMatches(LANG(?name), '{language}')
+                    FILTER(lang(?name) = '{language}')
                 }}
             """
 
@@ -29,16 +29,16 @@ def get_labels(entity, data):
         q = f"""
                 PREFIX wd: <http://www.wikidata.org/entity/>
                 PREFIX rdf: <http://www.w3.org/2000/01/rdf-schema#>
-                PREFIX rdf: <http://www.w3.org/2004/02/skos/core#>
-                PREFIX rdf: <http://schema.org/alternateName>
+                PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+                PREFIX schema: <http://schema.org/>
                 SELECT ?name
                 WHERE {{
                     {{ wd:{entity} rdf:label ?name. }}
                     UNION
-                     {{ wd:{entity} rdf:altLabel ?name. }}
+                     {{ wd:{entity} skos:altLabel ?name. }}
                     UNION
-                    {{ wd:{entity} ?p ?name. }}
-                    FILTER langMatches(LANG(?name), '{language}')
+                    {{ wd:{entity} schema:alternateName ?name. }}
+                    FILTER(lang(?name) = '{language}')
                 }}
             """
 
@@ -65,20 +65,20 @@ def get_descriptions(entity, data):
     def get_query(entity, language):
         q = f"""
                 PREFIX wd: <http://www.wikidata.org/entity/>
-                PREFIX rdf: <http://schema.org/description>
-                SELECT ?description     
-                WHERE {{ wd:{entity} ?p ?description.
-                    FILTER langMatches(LANG(?description), '{language}')
+                PREFIX schema: <http://schema.org/>
+                SELECT ?text    
+                WHERE {{ wd:{entity} schema:description ?text.
+                    FILTER(lang(?text) = '{language}')
                 }}
             """
 
         return q
 
     for r in data.query(get_query(entity, "cs")):
-        result["cs"] = r["description"]
+        result["cs"] = r["text"]
 
     for r in data.query(get_query(entity, "en")):
-        result["en"] = r["description"]
+        result["en"] = r["text"]
 
     return result
 
@@ -87,9 +87,9 @@ def get_ids(entity, data):
     result = {"wikidata": entity}
     q = f"""
             PREFIX wd: <http://www.wikidata.org/entity/>
-            PREFIX rdf: <http://www.wikidata.org/prop/direct/>
+            PREFIX prop: <http://www.wikidata.org/prop/direct/>
             SELECT ?id     
-            WHERE {{ wd:{entity} rdf:P646 ?id.
+            WHERE {{ wd:{entity} prop:P646 ?id.
             }}
         """
 
@@ -102,9 +102,9 @@ def get_ids(entity, data):
 def get_rankings(entity, data):
     q = f"""
                PREFIX wd: <http://www.wikidata.org/entity/>
-               PREFIX rdf: <http://wikiba.se/ontology#>
+               PREFIX wikibase: <http://wikiba.se/ontology#>
                SELECT ?num    
-               WHERE {{ wd:{entity} rdf:sitelinks ?num.
+               WHERE {{ wd:{entity} wikibase:sitelinks ?num.
                }}
         """
 
@@ -127,12 +127,12 @@ def get_type(entity, data):
     }
     q = f"""
             PREFIX wd: <http://www.wikidata.org/entity/>
-            PREFIX rdf: <http://www.wikidata.org/prop/direct/>
+            PREFIX prop: <http://www.wikidata.org/prop/direct/>
             SELECT ?class
             WHERE {{ 
-            {{ wd:{entity} rdf:P279 ?class. }}
+            {{ wd:{entity} prop:P279 ?class. }}
             UNION 
-            {{ wd:{entity} rdf:P31 ?class. }}
+            {{ wd:{entity} prop:P31 ?class. }}
             }}
         """
 
